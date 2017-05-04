dc896752a707ae684c1271a4fd7235d7886d041c
chezze911 <chinguyen@Chis-MacBook-Pro.local>
2017-05-04 14:37:20 -0700

    Update home, about us, and menu states in order to make use of resolve to supply data to controllers

--- Ionic/conFusion/www/js/services.js
+++ Ionic/conFusion/www/js/services.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('conFusion.services', ['ngResource'])
-        .constant("baseURL","http://localhost:3000/")
+        .constant("baseURL","http://10.21.82.170:3000/")
         .factory('menuFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
 
             return $resource(baseURL + "dishes/:id", null, {
